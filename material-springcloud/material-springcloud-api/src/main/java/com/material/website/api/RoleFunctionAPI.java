34d58a75882f73337080872d7fbcf8cfca7e75db
sunxiaorong <532192188@qq.com>
2017-07-24 22:45:10 +0800

    代码提交奥

---
+++ material-springcloud/material-springcloud-api/src/main/java/com/material/website/api/RoleFunctionAPI.java
@@ -0,0 +1,59 @@
+package com.material.website.api;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.material.website.dto.FunctionDto;
+import com.material.website.dto.RoleFunctionDto;
+import com.material.website.entity.Function;
+
+/**
+ * 角色功能业务接口
+ * @author sunxiaorong
+ *
+ */
+@RequestMapping("/RoleFunctionAPI")
+public interface RoleFunctionAPI {
+	
+	/**
+	 * 根据角色编号查询
+	 * @RequestParam roleId
+	 * @return
+	 */
+	@RequestMapping(value="/queryFunctionByRoleId",method=RequestMethod.GET)
+	public List<RoleFunctionDto> queryFunctionByRoleId(@RequestParam("roleId") Integer roleId);
+	
+	/**
+	 * 根据父功能编号查询功能列表
+	 * @RequestParam parentId
+	 * @return
+	 */
+	@RequestMapping(value="/queryFunctionListByParentId",method=RequestMethod.GET)
+	public List<FunctionDto> queryFunctionListByParentId(@RequestParam("parentId") Integer parentId);
+	
+	/**
+	 * 查询 所有 的一级功能
+	 * @return
+	 */
+	@RequestMapping(value="/queryAllFunction",method=RequestMethod.GET)
+	public List<FunctionDto> queryAllFunction();
+	
+	/**
+	 * 删除角色功能
+	 * @RequestParam id
+	 * @return
+	 */
+	@RequestMapping(value="/delRoleFunction",method=RequestMethod.GET)
+	public boolean delRoleFunction(@RequestParam("id") Integer id);
+	
+	/**
+	 * 根据id查询功能信息
+	 * @RequestParam id
+	 * @return
+	 */
+	@RequestMapping(value="/queryFunctionById",method=RequestMethod.GET)
+    public Function queryFunctionById(@RequestParam("id") Integer id);
+}
---
+++ material-springcloud/material-springcloud-api/src/main/java/com/material/website/api/SupplierAPI.java
@@ -0,0 +1,61 @@
+package com.material.website.api;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.material.website.dto.SupplierDto;
+import com.material.website.entity.Supplier;
+import com.material.website.system.Pager;
+
+/**
+ * 供应商客户端申明
+ * @author Sunxiaorong
+ *
+ */
+@RequestMapping("/SupplierAPI")
+public interface SupplierAPI {
+	
+	/**
+	 * 分页查询供应商信息
+	 * @RequestParam supplierArgs
+	 * @return
+	 */
+	@RequestMapping(value="/querySupplierList",method=RequestMethod.GET)
+	public Pager<SupplierDto> querySupplierList(@RequestParam Map<String, Object>map);
+	
+	/**
+	 * 添加供应商信息
+	 * @RequestParam supplier
+	 * @return
+	 */
+	@RequestMapping(value="/addSupplier",method=RequestMethod.GET)
+	public boolean addSupplier(@RequestParam Map<String, Object>map);
+	
+	/**
+	 * 修改供应商信息
+	 * @RequestParam supplier
+	 * @return
+	 */
+	@RequestMapping(value="/updateSupplier",method=RequestMethod.GET)
+	public void updateSupplier(@RequestParam Map<String, Object>map);
+	
+	/**
+	 * 根据供应商编号查询供应商信息
+	 * @RequestParam supplierId
+	 * @return
+	 */
+	@RequestMapping(value="/querySupplierById",method=RequestMethod.GET)
+	public Supplier querySupplierById(@RequestParam("supplierId") Integer supplierId);
+	
+	/**
+	 * 查询所有的供应商
+	 * @return
+	 */
+	@RequestMapping(value="/queryAllSupplier",method=RequestMethod.GET)
+	public List<Supplier> queryAllSupplier();
+	
+}
